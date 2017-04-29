7e2b011e9f5ee0b0ee724b8ff58f3f6eccc3e67a
eponymousjayk <jaykisonfire@gmail.com>
2017-04-29 13:18:52 -0500

    Deleted superfluous files

--- fib.hs
+++
@@ -1,7 +0,0 @@
-fib 0 = 1
-fib 1 = 1
-fib n = fib (n - 1) + fib (n - 2)
-
-fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-
-fib2 = (!!) fibs
--- sqrt.hs
+++
@@ -1,6 +0,0 @@
-(*^) x z
- | z < 0         = 1 / (x *^ (-z))
- | z < 0.0000001 = 1
- | otherwise     = sub * sub
-                   where sub = x *^ (z/2)
-
