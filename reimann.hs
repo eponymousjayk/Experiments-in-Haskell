2efe310220df8e827a8a174b9fcc369964e166c8
eponymousjayk <jaykisonfire@gmail.com>
2017-04-30 23:53:55 -0500

    Added gamma function approximation to Reimann.hs, reworked it.

--- reimann.hs
+++ reimann.hs
@@ -1,11 +1,19 @@
 -- Something to do with the reimann zeta function.
 
-epsilon = 0.0000000001
+epsilon = 0.000000000001
 
-zeta x = zeta' 1 0
-         where zeta' n c = if (diff < epsilon)
-                            then c
-                            else zeta' (n + 1) next
-                           where next = c + (1/(n**x))
-                                 diff' = next - c
-                                 diff = if diff' < 0 then (-diff') else (diff')
+zeta' x = sum $ takeWhile (>0) $ map (\y -> if (1/(y**x) < epsilon) then 0 else (1/(y**x))) [1,2..]
+
+
+-- from the haskell wiki
+-- https://wiki.haskell.org/Gamma_and_Beta_function
+
+cof = [76.18009172947146,-86.50532032941677,24.01409824083091,-1.231739572450155,0.001208650973866179,-0.000005395239384953]
+
+ser = 1.000000000190015
+
+gammaln xx = let tmp' = (xx+5.5) - (xx+0.5)*log(xx+5.5)
+                 ser' = ser + sum (zipWith (/) cof [xx+1..])
+             in -tmp' + log(2.5066282746310005 * ser' / xx)
+
+gamma = exp . gammaln
